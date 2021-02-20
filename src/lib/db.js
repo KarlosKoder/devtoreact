0b72e6a0d348541c81c443e052037af3a402e9a9
KarlosKoder <carlosemiliovenancio@gmail.com>
2021-02-19 18:01:50 -0600

    Running version 0

--- src/lib/db.js
+++ src/lib/db.js
@@ -8,7 +8,7 @@ const {
   DB_NAME
 } = process.env
 
-const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+const url = `mongodb+srv://KarlVN:8bnUk3B83Jma2NA@cluster0.rszt9.mongodb.net/kodemia`
 
 const connect = mongoose.connect(
   url, 
